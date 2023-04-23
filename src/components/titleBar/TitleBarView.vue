7d55b45752fc1219536192f86e5a6b264a3c1378
zhangweisong02 <zhangweisong02@meituan.com>
2023-04-23 16:20:05 +0800

    fix: 代码开发

---
+++ src/components/titleBar/TitleBarView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="title-bar">
+    title bar
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ src/page/PageView.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="page-view">
+    <div class="page-side"></div>
+    <div class="page-main"></div>
+    <div class="main-top"></div>
+    <div calss="main-bottom"></div>
+  </div>
+</template>
+
+<style>
+.page-view {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+}
+.page-side {
+  width: 20vw;
+  height: 100vh;
+}
+.page-main {
+  flex: 1;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+.main-top {
+  width: 100%;
+  height: 20vh;
+}
+.main-bottom {
+  width: 100%;
+  flex: 1;
+}
+</style>
