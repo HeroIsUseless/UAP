63c79796155764654637fc8fde04870dbd39cb39
zhangweisong02 <zhangweisong02@meituan.com>
2023-06-15 10:55:04 +0800

    fix: 代码处理

--- src/plugins/comba/Controller.ts
+++ src/plugins/comba/Controller.ts
@@ -1,3 +1,7 @@
-export const doNextProcess = (data: any) => {
+import combaModel from './Model'
 
+export const doNextProcess = (data: any) => {
+  combaModel.data.compIndex += 1
+  if (combaModel.data.compIndex >= 4) return
+  
 }
--- src/plugins/comba/components/inputView/view.vue
+++ src/plugins/comba/components/inputView/view.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="input-view">
     <div>
-      <Button @click="onSelectFileBtnClick">开始执行</Button>
+      <Button @click="onOkBtnClick">开始执行</Button>
     </div>
-    <div v-for="item in filePathsRef">
-      {{ item }}
+    <div>
+      <Input v-bind:value="value" />
     </div>
   </div>
 </template>
@@ -14,24 +14,10 @@ import { Button, Input } from 'ant-design-vue';
 import { doNextProcess } from '../../Controller';
 import { ipcRenderer } from 'electron';
 import { ref } from 'vue';
-let filePathsRef = ref<string[]>([])
-const onSelectFileBtnClick = async () => {
-  const filePaths = await openSelectDialog()
-  console.log('zws files', filePaths)
-  filePathsRef.value = filePaths
+let value = ref<string>('')
+const onOkBtnClick = async () => {
   doNextProcess({
-    filePaths
-  })
-}
-
-// 打开选择弹窗
-async function openSelectDialog(): Promise<string[]> {
-  return new Promise((resolve, reject) => {
-    ipcRenderer.send('open-select-dialog', {});
-    ipcRenderer.on('open-select-dialog-back', (event, arg) => {
-      console.log('zws 9762', arg); // 输出主进程传递的参数
-      resolve(arg.filePaths)
-    });
+    value: value.value
   })
 }
 
