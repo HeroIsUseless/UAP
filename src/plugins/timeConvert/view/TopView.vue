6eac1d8fed7e51355aa26c7cd855bcc98caee4ea
zhangweisong02 <zhangweisong02@meituan.com>
2023-06-16 15:42:33 +0800

    fix: 时间转换

--- src/plugins/timeConvert/view/TopView.vue
+++ src/plugins/timeConvert/view/TopView.vue
@@ -2,17 +2,28 @@
   <div class="top-view">
     <h1>时间戳转换</h1>
     <div class="content">
-      <Input v-model:value="formatTime">
-        <template #addonAfter>
-          <Select v-model:value="unit" style="width: 80px">
-            <SelectOption value="ms">ms</SelectOption>
-            <SelectOption value="s">s</SelectOption>
-            <SelectOption value="年-月-日">年-月-日</SelectOption>
-            <SelectOption value="时-分-秒">时-分-秒</SelectOption>
-          </Select>
-        </template>
-      </Input>
-      <Button>开始转换</Button>
+      <div>
+        <Input v-model:value="formatTime" placeholder="请在这里输入时间">
+        </Input>
+      </div>
+      <div class="margin-top-6px">
+        <Select v-model:value="unit" style="width: 80px">
+          <SelectOption value="ms">ms</SelectOption>
+          <SelectOption value="s">s</SelectOption>
+          <SelectOption value="年-月-日">年-月-日</SelectOption>
+          <SelectOption value="时-分-秒">时-分-秒</SelectOption>
+        </Select>
+        <span>&nbsp;转换为：</span>
+        <Select v-model:value="unit" style="width: 80px">
+          <SelectOption value="ms">ms</SelectOption>
+          <SelectOption value="s">s</SelectOption>
+          <SelectOption value="年-月-日">年-月-日</SelectOption>
+          <SelectOption value="时-分-秒">时-分-秒</SelectOption>
+        </Select>
+      </div>
+      <div class="margin-top-6px">
+        <Button type="primary" @click="onConverBtnClick">开始转换</Button>
+      </div>
     </div>
   </div>
 </template>
@@ -23,7 +34,18 @@ import { computed, ref } from 'vue';
 import model, {TimeType} from '../Model'
 
 const unit = ref(TimeType.ms)
-const formatTime = computed(()=>Math.floor(model.data.timestamp))
+const formatTime = ref(Date.now())
+const onConverBtnClick = () => {
+  console.log('zws 4423', filterTime(formatTime.value))
+}
+
+function filterTime(time: any) {
+  const date = new Date(time)
+  const Y = date.getFullYear()
+  const M = date.getMonth() + 1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1 
+  const D = date.getDate()
+  return `${Y}-${M}-${D}`
+}
 </script>
 
 <style scoped>
@@ -33,6 +55,9 @@ const formatTime = computed(()=>Math.floor(model.data.timestamp))
   padding: 12px;
 }
 .content {
-  display: flex;
+  
+}
+.margin-top-6px {
+  margin-top: 6px;
 }
 </style>
